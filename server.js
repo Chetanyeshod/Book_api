f27e972a5286d32bcc6da06d9cc853a034c30562
Chetanyeshod <122098137+Chetanyeshod@users.noreply.github.com>
2025-04-23 08:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const bookRoutes = require("./routes/bookRoutes");
 require('dotenv').config();
 
 const app = express();
+// Middleware
 app.use(express.json());
-
+// Routes
 app.use("/api", bookRoutes);
 
+// Connect DB and start server
 mongoose.connect(process.env.MONGODB_URI)
 
 app.listen(process.env.PORT || 3000,    console.log(`server is running on ${process.env.PORT || 3000}`))
